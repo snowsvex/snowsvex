40a7add77ff66ecc839e7937529f7e8e97f42e4b
Conor Sinclair <conor@sinclair.software>
2021-02-28 20:41:13 +0000

    basic CLI setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-console.log('Test')
+import program from 'commander'
+import figlet from 'figlet'
+import clear from 'clear'
+import chalk from 'chalk'
+import { version, description } from '../package.json'
+
+clear()
+console.log(chalk.cyan(figlet.textSync('Snowsvex', { horizontalLayout: 'full' })))
+
+program
+  .version(version)
+  .description(description)
+  .command('dev')
+  .description('Start Dev Mode!')
+  .action(() => {
+    console.log('dev mode')
+  })
+  .command('build')
+  .description('Build static assets!')
+  .action(() => {
+    console.log('build mode')
+  })
+  .parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
